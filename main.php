0e44b5040a186e3e7f5057f64816fd042cc4d283
Corsin <69308146+corsin1337@users.noreply.github.com>
2023-02-20 14:20:41 +0100

    Delete main.php

--- main.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('totp.class.php');
-
-if (isset($_GET['secretkey'])) {
-    $secret = $_GET['secretkey'];
-    $totp = new TOTP($secret);
-    echo $result = $totp->totp($secret);
-    $totp->reloadOnTime();
-}
